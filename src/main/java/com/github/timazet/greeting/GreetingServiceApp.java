6930cc2b108635bd5e68fe2abfc279747e83f1d4
timazet <14309920+timazet@users.noreply.github.com>
2020-12-15 13:48:09 +0300

    Intentional mistakes are added

--- src/main/java/com/github/timazet/greeting/GreetingServiceApp.java
+++ src/main/java/com/github/timazet/greeting/GreetingServiceApp.java
@@ -2,6 +2,7 @@ package com.github.timazet.greeting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
